97e943b9f38ed854db940ec8b57c93594ca74e02
Vladimir <v.korotkin11@gmail.com>
2018-09-22 18:35:20 +0300

    quadratic-equation: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+    let [a, , b, c] = equation.replace(/\s/g, "").match(/-?\d+/g);
+    b /= a;
+    c /= a;
+
+    return [
+        (-b - Math.sqrt(b * b - 4 * c)) / 2,
+        (-b + Math.sqrt(b * b - 4 * c)) / 2,
+    ];
+};
